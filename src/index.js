d40bc3e6e8814e05efce415cc18c332c4045ddff
Samuel Muigai <ndungu.muigai01@gmail.com>
2023-10-14 19:48:57 +0300

    Lab completed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,29 @@
-const init = () => {
-  
+const init = () => 
+{
+    //Getting the form from the HTML page
+    const form=document.querySelector("form")
+
+    //Adding an event listener to the form
+    form.addEventListener("submit", (event)=>
+    {
+        //Preventing the page from reloading once form is submitted
+        event.preventDefault()
+
+        //Getting the value from the input field
+        const input=document.getElementById("searchByID")
+
+        //Fetching data based on the value input by the user
+        fetch(`http://localhost:3000/movies/${input.value}`)
+            .then(response=> response.json())
+            .then(data=>
+                {
+                    const title=document.querySelector("section#movieDetails h4")
+                    const summary=document.querySelector("section#movieDetails p")
+
+                    title.innerText=data.title
+                    summary.innerText=data.summary
+                })
+    })
 }
 
 document.addEventListener('DOMContentLoaded', init);
